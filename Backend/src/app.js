36b7ffb36316bc107d23b793e7bd0bab76c16eea
pranavbhardwaj9122 <pranavbhardwaj9122professional@gmail.com>
2026-05-02 23:37:44 +0530

    Fixed CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin: "https://ai-resume-analyser-livid-nine.vercel.app",
-    credentials: true
-}))
+app.use(cors({origin: "https://ai-resume-analyser-livid-nine.vercel.app",credentials: true}))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")

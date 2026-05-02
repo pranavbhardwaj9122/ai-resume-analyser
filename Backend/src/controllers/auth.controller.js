f330307c595313e9014f934060ab828f57a30347
Pranav Bhardwaj <pranavbhardwaj9122professional@gmail.com>
2026-05-03 00:19:09 +0530

    Enhance JWT cookie security settings
    Updated cookie settings for JWT token to enhance security.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -42,7 +42,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d"}
     )
 
-    res.cookie("token", token)
+    res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,        // REQUIRED for SameSite='none' (HTTPS only)
+  sameSite: 'none',    // REQUIRED for cross-origin cookies
+  maxAge: 24 * 60 * 60 * 1000 // 1 day
+});
 
 
     res.status(201).json({
@@ -87,7 +92,13 @@ async function loginUserController(req, res) {
         { expiresIn: "1d"}
     )
 
-    res.cookie("token", token)
+    res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,        // REQUIRED for SameSite='none' (HTTPS only)
+  sameSite: 'none',    // REQUIRED for cross-origin cookies
+  maxAge: 24 * 60 * 60 * 1000 // 1 day
+});
+    
     res.status(200).json({
         message:"User loggedin successfully",
         user:{

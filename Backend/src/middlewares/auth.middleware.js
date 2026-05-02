6408aee6f42f9b0fbb4e797fbb9bcbaf8abe4481
Pranav Bhardwaj <pranavbhardwaj9122professional@gmail.com>
2026-05-03 00:01:27 +0530

    Add semicolon and log token in auth middleware

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,8 @@ const tokenBlacklistModel = require("../models/blacklist.model")
 
 async function authUser(req, res, next) {
 
-    const token = req.cookies.token
+    const token = req.cookies.token;
+    console.log(token);
     if (!token) {
         return res.status(401).json({
             message: "Token not provided."

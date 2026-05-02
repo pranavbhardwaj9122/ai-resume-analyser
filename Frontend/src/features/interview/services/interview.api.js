cca6f4a7fe302db02a1c46cf0daecfde4803a862
pranavbhardwaj9122 <pranavbhardwaj9122professional@gmail.com>
2026-05-02 22:57:19 +0530

    Changed Server Base URL in frontend

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-analyser-production-1f4b.up.railway.app",
     withCredentials: true,
 })
 
